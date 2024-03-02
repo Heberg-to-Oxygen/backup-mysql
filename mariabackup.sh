886294419445db9c03715d6c473b03466b636612
Florian.D <floriandjerbi@gmail.com>
2024-03-02 14:53:28 +0100

    Add log file in variable and script

--- mariabackup.sh
+++ mariabackup.sh
@@ -26,10 +26,10 @@ function backup_inc(){
     last_inc_number=$(echo "${last_inc_name}" | cut -f4 -d/ |cut -f3 -d- |cut -f1 -d.)
     new_inc_number=$((${last_inc_number}+1))
     if [[ -z ${last_inc_name} ]];then
-        echo "Run inc backup by last full !"
+        echo "Run inc backup by last full !" >> log_file
         mariabackup --backup --stream=mbstream --incremental-basedir=${folder_backup}/${backup_full_name}-${last_full_number} --user=root --extra-lsndir=${folder_backup}/${backup_inc_name}-${last_full_number}-${new_inc_number} | gzip > ${folder_backup}/${backup_inc_name}-${last_full_number}-${new_inc_number}.gz
     else
-        echo "Run inc backup by last inc !"
+        echo "Run inc backup by last inc !" >> log_file
         mariabackup --backup --stream=mbstream --incremental-basedir=${folder_backup}/${backup_inc_name}-${last_full_number}-${last_inc_number} --user=root --extra-lsndir=${folder_backup}/${backup_inc_name}-${last_full_number}-${new_inc_number} | gzip > ${folder_backup}/${backup_inc_name}-${last_full_number}-${new_inc_number}.gz
     fi
 }
@@ -37,20 +37,20 @@ function backup_inc(){
 function check_last_full(){
     last_full_name=$(find ${folder_backup} -type f -name ${backup_full_name}-*.gz -printf "%T@ %Tc %p\n" |sort -n |tail -n 1)
     if [[ -z ${last_full_name} ]];then
-        echo "Run first full backup !"
+        echo "Run first full backup !" >> log_file
 	last_full_number=0
         sleep 3
 	backup_full "${last_full_number}"
     else
         last_full_number=$(echo "${last_full_name}" | cut -f4 -d/ |cut -f2 -d- |cut -f1 -d.)
-        last_full_date=$(find ${folder_backup} -type f -name ${backup_full_name}-*.gz -mtime -${incremental_day} -printf "%T@ %Tc %p\n" | wc -l) 	# Prod
+        last_full_date=$(find ${folder_backup} -type f -name ${backup_full_name}-*.gz -mtime -${incremental_day}-1 -printf "%T@ %Tc %p\n" | wc -l) 	# Prod
         # last_full_date=$(find ${folder_backup} -type f -name ${backup_full_name}-*.gz -mmin -420 -printf "%T@ %Tc %p\n" | wc -l)			# Dev
         if [ ${last_full_date} -eq 0 ];then
-            echo "Run full backup !"
+            echo "Run full backup !" >> log_file
             sleep 3
             backup_full "${last_full_number}"
         else
-            echo "Run inc backup !"
+            echo "Run inc backup !" >> log_file
             sleep 3
             backup_inc "${last_full_number}"
         fi
@@ -65,7 +65,7 @@ function check_old_backup(){
 	if [ -n ${folder_backup} ];then
 	    rm -rf ${folder_backup}/${backup_full_name}-${old_full_number}*
 	    rm -rf ${folder_backup}/${backup_inc_name}-${old_full_number}*
-            echo "Delete full and incremental backup ${old_full_number}"
+            echo "Delete full and incremental backup ${old_full_number}" >> log_file
 	fi
     fi
 }
