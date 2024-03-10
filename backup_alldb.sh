53141f1b90fbaf46356b9071071068d6e49a1b5c
Florian.D <floriandjerbi@gmail.com>
2024-03-10 14:29:41 +0100

    refactor code

--- backup_alldb.sh
+++ backup_alldb.sh
@@ -18,7 +18,7 @@ last_log_file=${folder_backup}/backup_last.log
 #
 
 function msg(){
-    datetime_now=$(date +"%D %T" )
+    datetime_now=$(date +"%D %T")
     echo "[${datetime_now}] : $1" >> ${last_log_file}
 }
 
@@ -33,20 +33,20 @@ function init_script(){
 function backup_full(){
     last_full_number=$1
     last_full_number=$((${last_full_number}+1))
-    mariabackup --backup --stream=mbstream --user=${db_user} --password=${db_password} --extra-lsndir=${folder_backup}/${backup_full_name}-${last_full_number} | gzip > ${folder_backup}/${backup_full_name}-${last_full_number}.gz
+    mariabackup --backup --stream=mbstream --user=${db_user} --password=${db_password} --extra-lsndir=${folder_backup}/${backup_full_name}-${last_full_number} |gzip > ${folder_backup}/${backup_full_name}-${last_full_number}.gz
 }
 
 function backup_inc(){
     last_full_number=$1
     last_inc_name=$(find ${folder_backup} -type f -name ${backup_inc_name}-${last_full_number}*.gz -printf "%T@ %Tc %p\n" |sort -n |tail -n 1)
-    last_inc_number=$(echo "${last_inc_name}" | cut -f4 -d/ |cut -f3 -d- |cut -f1 -d.)
+    last_inc_number=$(echo "${last_inc_name}" |cut -f4 -d/ |cut -f3 -d- |cut -f1 -d.)
     new_inc_number=$((${last_inc_number}+1))
     if [[ -z ${last_inc_name} ]];then
         msg "Run incremental backup by last full !"
-        mariabackup --backup --stream=mbstream --user=${db_user} --password=${db_password} --incremental-basedir=${folder_backup}/${backup_full_name}-${last_full_number} --extra-lsndir=${folder_backup}/${backup_inc_name}-${last_full_number}-${new_inc_number} | gzip > ${folder_backup}/${backup_inc_name}-${last_full_number}-${new_inc_number}.gz
+        mariabackup --backup --stream=mbstream --user=${db_user} --password=${db_password} --incremental-basedir=${folder_backup}/${backup_full_name}-${last_full_number} --extra-lsndir=${folder_backup}/${backup_inc_name}-${last_full_number}-${new_inc_number} |gzip > ${folder_backup}/${backup_inc_name}-${last_full_number}-${new_inc_number}.gz
     else
         msg "Run incremental backup by last incremental !"
-        mariabackup --backup --stream=mbstream --user=${db_user} --password=${db_password} --incremental-basedir=${folder_backup}/${backup_inc_name}-${last_full_number}-${last_inc_number} --extra-lsndir=${folder_backup}/${backup_inc_name}-${last_full_number}-${new_inc_number} | gzip > ${folder_backup}/${backup_inc_name}-${last_full_number}-${new_inc_number}.gz
+        mariabackup --backup --stream=mbstream --user=${db_user} --password=${db_password} --incremental-basedir=${folder_backup}/${backup_inc_name}-${last_full_number}-${last_inc_number} --extra-lsndir=${folder_backup}/${backup_inc_name}-${last_full_number}-${new_inc_number} |gzip > ${folder_backup}/${backup_inc_name}-${last_full_number}-${new_inc_number}.gz
     fi
 }
 
@@ -58,10 +58,9 @@ function check_last_full(){
         sleep 3
 	backup_full "${last_full_number}"
     else
-        last_full_number=$(echo "${last_full_name}" | cut -f4 -d/ |cut -f2 -d- |cut -f1 -d.)
+        last_full_number=$(echo "${last_full_name}" |cut -f4 -d/ |cut -f2 -d- |cut -f1 -d.)
 	((incremental_day-=1))
-        last_full_count=$(find ${folder_backup} -type f -name ${backup_full_name}-*.gz -mtime -${incremental_day} -printf "%T@ %Tc %p\n" | wc -l)
-        echo "find ${folder_backup} -type f -name ${backup_full_name}-*.gz -mtime -${incremental_day} -printf "%T@ %Tc %p\n" | wc -l)"
+        last_full_count=$(find ${folder_backup} -type f -name ${backup_full_name}-*.gz -mtime -${incremental_day} -printf "%T@ %Tc %p\n" |wc -l)
         find ${folder_backup} -type f -name ${backup_full_name}-*.gz -mtime -${incremental_day} -printf "%T@ %Tc %p\n"
         if [ ${last_full_count} -eq 0 ];then
             msg "Run full backup !"
@@ -75,7 +74,7 @@ function check_last_full(){
 }
 
 function check_old_backup(){
-    count_full_backup=$(find ${folder_backup} -type f -name ${backup_full_name}-*.gz| wc -l)
+    count_full_backup=$(find ${folder_backup} -type f -name ${backup_full_name}-*.gz |wc -l)
     if [ ${count_full_backup} -gt ${full_retention} ]; then
         old_full_number=$(find ${folder_backup} -type f -name ${backup_full_name}-*.gz -printf "%T@ %Tc %p\n" |sort -n |head -n 1 |cut -f4 -d/ |cut -f2 -d- |cut -f1 -d.)
 	if [ -n ${folder_backup} ] && [ -n ${backup_full_name} ] && [ -n ${old_full_number} ] ;then
