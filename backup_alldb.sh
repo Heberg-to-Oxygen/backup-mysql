3e2b5a1f7444dd54fe5a0fa3b89b92a4714794fb
Florian.D <floriandjerbi@gmail.com>
2024-03-15 20:15:46 +0100

    Patch mariabackup and reorg code

--- backup_alldb.sh
+++ backup_alldb.sh
@@ -3,7 +3,7 @@
 # Author : DJERBI Florian
 # Object : Run full backup and incremental backup for all mariadb
 # Creation Date : 01/04/2024
-# Modification Date : 03/12/2024
+# Modification Date : 03/15/2024
 ###########################
 
 #
@@ -30,28 +30,6 @@ function init_script(){
     fi
 }
 
-function backup_full(){
-    last_full_number=$1
-    last_full_number=$((${last_full_number}+1))
-#    mariabackup --backup --stream=mbstream --user=${db_user} --password=${db_password} --extra-lsndir=${folder_backup}/${backup_full_name}-${last_full_number}
-    mariabackup --backup --user=${db_user} --password=${db_password} --target-dir=${folder_backup}/${backup_full_name}-${last_full_number}
-    tar -cvzf ${folder_backup}/${backup_full_name}-${last_full_number}.tar.gz ${folder_backup}/${backup_full_name}-${last_full_number} 
-}
-
-function backup_inc(){
-    last_full_number=$1
-    last_inc_name=$(find ${folder_backup} -type f -name ${backup_inc_name}-${last_full_number}*.gz -printf "%T@ %Tc %p\n" |sort -n |tail -n 1)
-    last_inc_number=$(echo "${last_inc_name}" |cut -f4 -d/ |cut -f3 -d- |cut -f1 -d.)
-    new_inc_number=$((${last_inc_number}+1))
-    if [[ -z ${last_inc_name} ]];then
-        msg "Run incremental backup by last full !"
-        mariabackup --backup --stream=mbstream --user=${db_user} --password=${db_password} --incremental-basedir=${folder_backup}/${backup_full_name}-${last_full_number} --extra-lsndir=${folder_backup}/${backup_inc_name}-${last_full_number}-${new_inc_number}
-    else
-        msg "Run incremental backup by last incremental !"
-        mariabackup --backup --stream=mbstream --user=${db_user} --password=${db_password} --incremental-basedir=${folder_backup}/${backup_inc_name}-${last_full_number}-${last_inc_number} --extra-lsndir=${folder_backup}/${backup_inc_name}-${last_full_number}-${new_inc_number}
-    fi
-}
-
 function check_last_full(){
     last_full_name=$(find ${folder_backup} -type f -name ${backup_full_name}-*.gz -printf "%T@ %Tc %p\n" |sort -n |tail -n 1)
     if [[ -z ${last_full_name} ]];then
@@ -62,17 +40,43 @@ function check_last_full(){
     else
         last_full_number=$(echo "${last_full_name}" |cut -f4 -d/ |cut -f2 -d- |cut -f1 -d.)
 	((incremental_day-=1))
-        last_full_count=$(find ${folder_backup} -type f -name ${backup_full_name}-*.gz -mtime -${incremental_day} -printf "%T@ %Tc %p\n" |wc -l)
-        find ${folder_backup} -type f -name ${backup_full_name}-*.gz -mtime -${incremental_day} -printf "%T@ %Tc %p\n"
+        last_full_count=$(find ${folder_backup} -type f -name ${backup_full_name}-* -mtime -${incremental_day} -printf "%T@ %Tc %p\n" |wc -l)
         if [ ${last_full_count} -eq 0 ];then
             msg "Run full backup !"
             sleep 3
-            backup_full "${last_full_number}"
+            # backup_full "${last_full_number}"
         else
+            msg "Run inc backup !"
             sleep 3
             backup_inc "${last_full_number}"
         fi
     fi
+}
+
+function backup_full(){
+    last_full_number=$1
+    last_full_number=$((${last_full_number}+1))
+    mariabackup --backup --user=${db_user} --password=${db_password} --target-dir=${folder_backup}/${backup_full_name}-${last_full_number} >> ${last_log_file} 2>&1
+    msg "Run tar gz last backup ${backup_full_name}-${last_full_number}"
+    tar -cvzf ${folder_backup}/${backup_full_name}-${last_full_number}.tar.gz -P ${folder_backup}/${backup_full_name}-${last_full_number} >> ${last_log_file}
+}
+
+function backup_inc(){
+    last_full_number=$1
+    last_inc_name=$(find ${folder_backup} -type f -name ${backup_inc_name}-${last_full_number}* -printf "%T@ %Tc %p\n" |sort -n |tail -n 1)
+    last_inc_number=$(echo "${last_inc_name}" |cut -f4 -d/ |cut -f3 -d- |cut -f1 -d.)
+    new_inc_number=$((${last_inc_number}+1))
+    if [[ -z ${last_inc_name} ]];then
+        msg "Run incremental backup by last full !"
+        mariabackup --backup --user=${db_user} --password=${db_password} --incremental-basedir=${folder_backup}/${backup_full_name}-${last_full_number} --target-dir=${folder_backup}/${backup_inc_name}-${last_full_number}-${new_inc_number} >> ${last_log_file} 2>&1
+    else
+        msg "Run incremental backup by last incremental !"
+        mariabackup --backup --user=${db_user} --password=${db_password} --incremental-basedir=${folder_backup}/${backup_inc_name}-${last_full_number}-${last_inc_number} --target-dir=${folder_backup}/${backup_inc_name}-${last_full_number}-${new_inc_number} >> ${last_log_file} 2>&1
+    fi
+    tar -cvzf ${folder_backup}/${backup_inc_name}-${last_full_number}-${new_inc_number}.tar.gz -P ${folder_backup}/${backup_inc_name}-${last_full_number}-${new_inc_number} >> ${last_log_file}
+}
+
+function delete_last_folder_backup(){
 }
 
 function check_old_backup(){
@@ -90,7 +94,7 @@ function check_old_backup(){
 function sync_s3(){
     if [ ${s3_backup} == "yes" ];then
 	msg "Sync backup into S3"
-	aws s3 sync ${folder_backup} s3://${s3_name}/${s3_path} >> ${last_log_file}
+	aws s3 sync --exclude "*" --include "*.tar.gz" --include "*.log" ${folder_backup} s3://${s3_name}/${s3_path} >> ${last_log_file}
         s3_retention_date=$(date +"%Y-%m-%d %T" -d "-${s3_retention_day} days")
 	s3_old_backup=$(aws s3 ls --recursive s3://h2o-backup-mysql/lab1.ta-info.net/ |grep "${backup_full_name}" |grep gz |awk -v prev="${s3_retention_date}" '$0 < prev {print $4}') 
 	s3_old_full_number=$(echo ${s3_old_backup} |cut -f2 -d/|cut -f2 -d- |cut -f1 -d.)
@@ -107,10 +111,11 @@ function last_log_in_log(){
 }
 
 function main(){
-    datetime_now=$(date +"%D %T" )
-    echo "[${datetime_now}] : Run script backup mariadb" > ${last_log_file}
+    datetime_now=$(date +"%D %T")
+    echo  "[${datetime_now}] : Run script backup mariadb" > ${last_log_file}
     init_script "$@"
     check_last_full "$@"
+    delete_last_folder_backup "$@"
     check_old_backup "$@"
     sync_s3 "$@"
     last_log_in_log "$@"
